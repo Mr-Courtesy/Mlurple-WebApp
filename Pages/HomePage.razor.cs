396015aa9c1d7bd87ecd10c1ebb79033a435219b
Eric Martinez-Velazquez <auesomeg@gmail.com>
2021-03-12 20:02:09 +0000

    Add logic for getting a users projects when the page is loaded.
    Calls the api and gets a users project spaces. It adds the projects to the SessionUser.projects and makes sure to not add duplicate ones.

--- Pages/HomePage.razor.cs
+++ Pages/HomePage.razor.cs
@@ -1,11 +1,53 @@
 using Microsoft.AspNetCore.Components;
 using Mlurple_WebApp.Models;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System;
+using Mlurple_WebApp.Services;
 
 namespace Mlurple_WebApp.Pages
 {
     public class HomePageBase : ComponentBase
     {
-        
+        public static bool hasProjects { get; set; }
+        public static string UserProjectResponse { get; set; }
+        protected override void OnInitialized()
+        {
+            if (Session.isAuthorized)
+            {
+                string encryptedUsername = EncryptAndDecryptService.Encrypt("key", SessionUser.username);
+
+                HttpClient client = new HttpClient();
+                HttpRequestMessage request = new HttpRequestMessage()
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://mysupersecretapi.com/api/ProjectSpace?username={encryptedUsername}")
+                };
+
+                using (var response = client.Send(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = response.Content.ReadAsStringAsync().Result;
+
+                    if (body == "No projects found.")
+                    {
+                        hasProjects = false;
+                    }
+                    else 
+                    {
+                        hasProjects = true;
+                        var projects = body.Split("&");
+
+                        foreach (var proj in projects)
+                        {
+                            if (proj != null && !SessionUser.projects.Contains(proj))
+                            {
+                                SessionUser.projects.Add(proj);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
