2250160d0d0f905eab5fe9a3c343f8825ee06dbd
Eric Martinez-Velazquez <auesomeg@gmail.com>
2021-03-12 06:29:47 +0000

    Add: Proper restrictions for registering users based off of username length and non-alphanumeric characters.
    Users will not be registered if their username is less than 5 characters or more than 15 characters. Usernames cannot contain non-alphanumeric characters and passwords cannot contain whitespace characters.

--- Pages/RegisterPage.razor.cs
+++ Pages/RegisterPage.razor.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using System;
+using System.Linq;
 using Mlurple_WebApp.Services;
 using Mlurple_WebApp.Models;
 using System.Net.Http;
@@ -13,29 +14,69 @@ namespace Mlurple_WebApp.Pages
         public string RegisterStatus;
         public async void HandleValidSubmit()
         {
-            string encryptedUsername = EncryptAndDecryptService.Encrypt("key goes here", userModel.Username);
-            string encryptedPassword = EncryptAndDecryptService.Encrypt("key goes here", userModel.Password);
+            char[] _username = userModel.Username.ToCharArray();
+            bool isValidUsername = _username.All(Char.IsLetterOrDigit);
+            bool usernameIsLongEnough = _username.Length >= 5;
+            bool usernameIsTooLong = _username.Length > 15;
 
-            HttpClient client = new HttpClient();
-            HttpRequestMessage request = new HttpRequestMessage()
+            if (!isValidUsername)
             {
-                Method = HttpMethod.Post,
-                RequestUri = new
-                Uri($"https://mysupersecretapi.com/api/User?username={encryptedUsername}&&password={encryptedPassword}")
-            };
-            using (var response = await client.SendAsync(request))
+                RegisterStatus = "Username can only contain alphanumeric characters.";
+            }
+            if (!usernameIsLongEnough)
             {
-                response.EnsureSuccessStatusCode();
-                var body = response.Content.ReadAsStringAsync();
-                if (body.Result == "User was succesfully registered.")
+                RegisterStatus = "Username must be at least 5 characters long.";
+            }
+            if (usernameIsTooLong)
+            {
+                RegisterStatus = "Username cannot be more than 10 characters long.";
+            }
+
+            char[] _password = userModel.Password.ToCharArray();
+            bool passwordIsLongEnough = _password.Length >= 6;
+        
+            if (!passwordIsLongEnough)
+            {
+                RegisterStatus = "Password must be at least 6 characters long.";
+            }
+
+            if (_password.Contains(' '))
+            {
+                RegisterStatus = "Password cannot contain whitespace characters.";
+            }
+            else if (isValidUsername)
+            {
+                if (passwordIsLongEnough)
                 {
-                    SessionUser.username = userModel.Username;
-                    RegisterStatus = SessionUser.username;
-                    RegisterStatus = body.Result;
+                    string encryptedUsername = EncryptAndDecryptService.Encrypt("key goes here", userModel.Username);
+                    string encryptedPassword = EncryptAndDecryptService.Encrypt("key goes here", userModel.Password);
+
+                    HttpClient client = new HttpClient();
+                    HttpRequestMessage request = new HttpRequestMessage()
+                    {
+                        Method = HttpMethod.Post,
+                        RequestUri = new
+                        Uri($"https://mysupersecretapi.com/api/User?username={encryptedUsername}&&password={encryptedPassword}")
+                    };
+                    using (var response = await client.SendAsync(request))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        var body = response.Content.ReadAsStringAsync();
+                        if (body.Result == "User was succesfully registered.")
+                        {
+                            SessionUser.username = userModel.Username;
+                            RegisterStatus = SessionUser.username;
+                            RegisterStatus = body.Result;
+                        }
+                        else
+                        {
+                            RegisterStatus = body.Result;
+                        }
+                    }
                 }
-                else if (body.Result == "User already exists with that username.")
+                else if (!passwordIsLongEnough)
                 {
-                    RegisterStatus = body.Result;
+                    RegisterStatus = "Password must be at least 6 characters long.";
                 }
             }
         }
