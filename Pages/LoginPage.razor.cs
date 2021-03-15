105d7f9e071e2d4797bcf8800cefb09b73b6c9bc
Eric Martinez-Velazquez <auesomeg@gmail.com>
2021-03-15 18:08:43 +0000

    Add better handling for logins, edit using directives.
    Adds the new encryption service via the package. Also checks for the same booleans as the register page, so rather than calling the api with a username or password that can't even be registered, it doesn't call the api at all and instead just tells the user that the username or password is incorrect, since it'd be impossible for someone to have that username and or password.

--- Pages/LoginPage.razor.cs
+++ Pages/LoginPage.razor.cs
@@ -1,45 +1,109 @@
 using Microsoft.AspNetCore.Components;
-using Mlurple_WebApp.Models;
+using Mlurple_WebApp.Classes;
 using System.Net.Http;
 using System;
-using Mlurple_WebApp.Services;
+using NETCore.Encrypt;
+using System.Linq;
+
 
 namespace Mlurple_WebApp.Pages
 {
     public class LoginPageBase : ComponentBase
     {
-        public string LoginStatus;
+
+        protected string LoginStatus;
         public UserModel userModel = new UserModel();
         [Inject]
         public NavigationManager NavManager { get; set; }
+
         public async void HandleValidSubmit()
         {
-            string encryptedUsername = EncryptAndDecryptService.Encrypt("key goes here", userModel.Username);
-            string encryptedPassword = EncryptAndDecryptService.Encrypt("key goes here", userModel.Password);
+            char[] _username = userModel.Username.ToCharArray();
+            bool isValidUsername = _username.All(Char.IsLetterOrDigit);
+            bool hasOnlyNumbers = _username.All(char.IsNumber);
+            bool usernameIsLongEnough = _username.Length >= 3;
+            bool usernameIsTooLong = _username.Length > 15;
+            char[] _password = userModel.Password.ToCharArray();
+            bool passwordIsLongEnough = _password.Length >= 6;
+            bool passwordIsNotValid = _password.All(Char.IsNumber);
+            bool passwordHasWhitespace = _password.Contains(' ');
+            bool passwordIsTooLong = _password.Length > 20;
+            if (!isValidUsername)
+            {
+                LoginStatus = "Username or password is incorrect.";
+            }
+            if (hasOnlyNumbers)
+            {
+                LoginStatus = "Username or password is incorrect.";
+            }
+            if (!usernameIsLongEnough)
+            {
+                LoginStatus = "Username or password is incorrect.";
+            }
+            if (usernameIsTooLong)
+            {
+                LoginStatus = "Username or password is incorrect.";
+            }
+            if (passwordIsNotValid)
+            {
+                LoginStatus = "Username or password is incorrect.";
+            }
+            if (!passwordIsLongEnough)
+            {
+                LoginStatus = "Username or password is incorrect.";
+            }
 
-            HttpClient client = new HttpClient();
-            HttpRequestMessage request = new HttpRequestMessage()
+            if (passwordHasWhitespace)
             {
-                Method = HttpMethod.Get,
-                RequestUri = new
-                Uri($"https://mysupersecretapi.com/api/User?username={encryptedUsername}&&password={encryptedPassword}")
-            };
-            using (var response = await client.SendAsync(request))
+                LoginStatus = "Username or password is incorrect.";
+            }
+
+            if (passwordIsTooLong)
             {
-                response.EnsureSuccessStatusCode();
-                var body = response.Content.ReadAsStringAsync();
-                if (body.Result == "true")
+                LoginStatus = "Username or password is incorrect.";
+            }
+            var aeskey = EncryptProvider.CreateAesKey();
+            var key = aeskey.Key;
+
+            string encryptedUsername = EncryptProvider.AESEncrypt(userModel.Username, "key");
+            string encryptedPassword = EncryptProvider.AESEncrypt(userModel.Password, "key");
+
+            if (isValidUsername && usernameIsLongEnough && !hasOnlyNumbers && passwordIsLongEnough && !passwordIsNotValid && !passwordHasWhitespace && !passwordIsTooLong)
+            {
+                HttpClient client = new HttpClient();
+                HttpRequestMessage request = new HttpRequestMessage()
                 {
-                    LoginStatus = "";
-                    SessionUser.username = userModel.Username;
-                    Session.isAuthorized = true;
-                    LoginStatus = $"{body.Result}: Hello, {SessionUser.username}.";
-                    NavManager.NavigateTo("/home");
-                }
-                else if (body.Result == "false")
+                    Method = HttpMethod.Get,
+                    RequestUri = new
+                    Uri($"https://mysupersecretapi.com/api/User?username={encryptedUsername}&&password={encryptedPassword}")
+                };
+                using (var response = await client.SendAsync(request))
                 {
-                    Session.isAuthorized = false;
-                    LoginStatus = $"{body.Result}: Username or password is incorrect {userModel.Username} {userModel.Password}";
+                    response.EnsureSuccessStatusCode();
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var body = response.Content.ReadAsStringAsync();
+                        if (body.Result == "true")
+                        {
+                            SessionUser.username = userModel.Username;
+                            Session.isAuthorized = true;
+                            NavManager.NavigateTo("/home");
+                        }
+                        else if (body.Result == "false")
+                        {
+                            Session.isAuthorized = false;
+                            LoginStatus = $"{body.Result}: Username or password is incorrect {userModel.Username} {userModel.Password}";
+                        }
+                        else
+                        {
+                            Session.isAuthorized = false;
+                            LoginStatus = "Whoops! An error occured.";
+                        }
+                    }
+                    else if (!response.IsSuccessStatusCode)
+                    {
+                        LoginStatus = "An error occured.";
+                    }
                 }
             }
         }
