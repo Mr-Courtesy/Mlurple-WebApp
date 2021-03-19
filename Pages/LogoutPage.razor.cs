36d97cd9e48fcaa2469bf0b1fcbc4bab2923bc4b
Eric Martinez-Velazquez <auesomeg@gmail.com>
2021-03-19 21:23:35 +0000

    Clear local storage when user logs out.

--- Pages/LogoutPage.razor.cs
+++ Pages/LogoutPage.razor.cs
@@ -1,17 +1,20 @@
 using Microsoft.AspNetCore.Components;
 using Mlurple_WebApp.Classes;
+using Blazored.LocalStorage;
+using System.Threading.Tasks;
 
 namespace Mlurple_WebApp.Pages
 {
     public class LogoutBase : ComponentBase
     {
         [Inject]
+        protected ILocalStorageService StorageService { get; set; }
+        [Inject]
         public NavigationManager NavManager { get; set; }
-        protected override void OnInitialized()
+        protected override async Task OnInitializedAsync()
         {
-            Session.isAuthorized = false;
+            await StorageService.ClearAsync();
             NavManager.NavigateTo("/");
-            SessionUser.projects.Clear();
         }
     }
 }
