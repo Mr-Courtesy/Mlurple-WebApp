d724085f312f98c897a60c909b21d0babed29382
Eric Martinez-Velazquez <auesomeg@gmail.com>
2021-03-19 21:20:36 +0000

    Add task for getting user projects, gets called if there is a user in session.

--- Pages/HomePage.razor.cs
+++ Pages/HomePage.razor.cs
@@ -6,61 +6,57 @@ using System;
 using Microsoft.AspNetCore.Components.Web;
 using NETCore.Encrypt;
 using Blazored.LocalStorage;
+using System.Collections.Generic;
 
 namespace Mlurple_WebApp.Pages
 {
     public class HomePageBase : ComponentBase
     {
-        protected string CurrentUser;
+        protected string Result;
         [Inject]
         protected ILocalStorageService StorageService { get; set; }
         [Inject]
         public NavigationManager navigationManager { get; set; }
         public static bool hasProjects { get; set; }
-        protected bool _authState { get; set; }
-        protected override async Task OnInitializedAsync()
+        protected string _authState { get; set; }
+        
+        protected async Task<List<string>> GetUserProjects(string username)
         {
-            CurrentUser = await StorageService.GetItemAsync<string>("username");
-            bool authState = await StorageService.GetItemAsync<bool>("authorized"); 
-            authState = _authState;
-            if (authState)
+            string encryptedUsername = EncryptProvider.AESEncrypt(username, "key");
+
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage()
             {
-                string username = await StorageService.GetItemAsync<string>("username");
-                string encryptedUsername = EncryptProvider.AESEncrypt(username, "key");
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://mysupersecretapi.com/api/ProjectSpace?username={encryptedUsername}")
+            };
 
-                HttpClient client = new HttpClient();
-                HttpRequestMessage request = new HttpRequestMessage()
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = response.Content.ReadAsStringAsync().Result;
+                if (body == "No projects found.")
                 {
-                    Method = HttpMethod.Get,
-                    RequestUri = new Uri($"https://mysupersecretapi.com/api/ProjectSpace?username={encryptedUsername}")
-                };
-
-                using (var response = client.Send(request))
+                    await StorageService.SetItemAsync("projects", "No projects found");
+                }
+                else
                 {
-                    response.EnsureSuccessStatusCode();
-                    var body = response.Content.ReadAsStringAsync().Result;
+                    var projects = body.Split("&");
+                    List<string> userProjects = new List<string>();
 
-                    if (body == "No projects found.")
+                    foreach (var proj in projects)
                     {
-                        hasProjects = false;
-                    }
-                    else 
-                    {
-                        hasProjects = true;
-                        var projects = body.Split("&");
-
-                        foreach (var proj in projects)
+                        if (proj != null && !userProjects.Contains(proj))
                         {
-                            if (proj != null && !SessionUser.projects.Contains(proj))
-                            {
-                                SessionUser.projects.Add(proj);
-                            }
+                            userProjects.Add(proj);
                         }
                     }
+                    await StorageService.SetItemAsync("projects", userProjects);
                 }
+                List<string> projs = await StorageService.GetItemAsync<List<string>>("projects");
+                return projs;
             }
         }
-
         public void OpenCreationPage(MouseEventArgs e)
         {
             navigationManager.NavigateTo("/create");
